b0f6459874a8f15d3c3771f336d684973b9b945a
senoryan <86090633+mayskij@users.noreply.github.com>
2021-10-20 11:46:23 +0300

    test1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [email, setEmail] = React.useState("")
+  const [name1, setName] = React.useState("")
+  const [phone, setPhone] = React.useState("")
+  const [emailDart, setEmailDart] = React.useState(false)
+  const [nameDart, setNameDart,] = React.useState(false)
+  const [phoneDart, setPhoneDart] = React.useState(false)
+  const [emailError, setEmailError] = React.useState("Введите корректный email")
+  const [nameError, setNameError] = React.useState("Введите корректное имя")
+  const [phoneError, setPhoneError] = React.useState("Введите корректный номер")
+  const [formValid, setFormValid] = React.useState(false)
+
+  React.useEffect(() => {
+    if (emailError || nameError || phoneError) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [emailError, nameError, phoneError])
+
+  const funkEmail = (e) => {
+    setEmail(e.target.value)
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Некорректный email")
+    } else {
+      setEmailError("")
+    }
+  }
+
+  const funkPhone = (e) => {
+    setPhone(e.target.value)
+    if (e.target.value.length > 11) {
+      setPhoneError("Некорректный номер")
+    } else {
+      setPhoneError("")
+    }
+  }
+
+  const funkName = (e) => {
+    setName(e.target.value)
+    if (e.target.value.length < 3) {
+      setNameError("Некорректное имя")
+    } else {
+      setNameError("")
+    }
+  }
+
+  const funk = (e) => {
+    switch (e.target.name) {
+      case "name1":
+        setNameDart(true)
+        break
+      case "phone":
+        setPhoneDart(true)
+        break
+      case "mail":
+        setEmailDart(true)
+        break
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="app">
+      <h1>Регистрация</h1>
+      <div>Имя</div>
+      {(nameDart && nameError) && <div style={{ color: "red" }}>{nameError}</div>}
+      <input onChange={e => funkName(e)} value={name1} onBlur={e => funk(e)} name="name1" type="text" placeholder="ваше имя" />
+      <div>Email</div>
+      {(emailDart && emailError) && <div style={{ color: "red" }}>{emailError}</div>}
+      <input onChange={e => funkEmail(e)} value={email} onBlur={e => funk(e)} name="mail" type="text" placeholder="ваш email" />
+      <div>Номер телефона</div>
+      {(phoneDart && phoneError) && <div style={{ color: "red" }}>{phoneError}</div>}
+      <input onChange={e => funkPhone(e)} value={phone} onBlur={e => funk(e)} name="phone" type="text" placeholder="ваш номер" />
+      <div>Языки</div>
+      <select>
+        <option>Английский</option>
+        <option>Русский</option>
+        <option>Китайский</option>
+      </select>
+      <button disabled={!formValid} type="submit">Зрегестрироваться</button>
+    </form>
   );
 }
 
